c49e3932b8095c4666c20f694b281898d71b75f6
Hamed Asadi Asl <90451086+hamed3958@users.noreply.github.com>
2021-10-10 15:25:49 +0330

    set hover and dark mode stay

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
+"use strict"
+
 const toggle = document.getElementById('toggle');
 const body = document.querySelector('body');
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  toggle.classList.add('active');
+  body.classList.add("active");
+}
 
 toggle.onclick = function () {
-    toggle.classList.toggle('active');
-    body.classList.toggle('active');
+  toggle.classList.toggle('active');
+  body.classList.toggle('active');
+
+  let theme = "light";
+  if (body.classList.contains('active')) {
+    theme = "dark";
+  }
+
+  localStorage.setItem("theme", theme);
 }
